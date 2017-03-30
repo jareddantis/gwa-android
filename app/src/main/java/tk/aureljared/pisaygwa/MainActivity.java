7bc05f91d2b7831ec59b13f5a46a1411c4504cf9
Aurel Jared Dantis <jareddantis@gmail.com>
2017-03-30 22:54:49 +0800

    Initial commit

---
+++ app/src/main/java/tk/aureljared/pisaygwa/MainActivity.java
@@ -0,0 +1,27 @@
+package tk.aureljared.pisaygwa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // WebView
+        WebView mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebContentsDebuggingEnabled(true);
+        mWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
