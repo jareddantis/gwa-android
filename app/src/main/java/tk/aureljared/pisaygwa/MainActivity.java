a14ed5b2bf04502f361d48e942db2ae358234421
Jared Dantis <jareddantis@gmail.com>
2017-07-27 17:22:35 +0800

    Release 6
    - Enable hardware acceleration
    - Prevent app reload on orientation change
    - Update web app to
    [08c0f1d](https://github.com/illustra/gwa/commit/08c0f1dc1e17954e48f0fb7c3ff22e43f9eace34)

--- app/src/main/java/tk/aureljared/pisaygwa/MainActivity.java
+++ app/src/main/java/tk/aureljared/pisaygwa/MainActivity.java
@@ -2,6 +2,7 @@ package tk.aureljared.pisaygwa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -10,18 +11,41 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
 
+    private String gwaUrl = "file:///android_asset/www/index.html";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // WebView
-        WebView mWebView = (WebView) findViewById(R.id.activity_main_webview);
-        WebSettings webSettings = mWebView.getSettings();
+        // WebView settings
+        webView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
-        mWebView.setWebChromeClient(new WebChromeClient());
-        mWebView.setWebContentsDebuggingEnabled(true);
-        mWebView.loadUrl("file:///android_asset/www/index.html");
+        webSettings.setSupportZoom(false);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebContentsDebuggingEnabled(false);
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
+        if (savedInstanceState == null)
+            webView.loadUrl(gwaUrl);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webView.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        webView.restoreState(savedInstanceState);
     }
 }
